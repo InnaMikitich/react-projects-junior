012300b21b7d874fc9d0d43ab6483ae80f42d072
InnaMikitich <i.seergee@gmail.com>
2025-03-19 15:15:14 +0300

    save changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-import React from 'react'
+import React from 'react';
+import './index.scss';
+import {Collection} from './Collection';
+
+
 
 function App() {
+  const [collection, setCollection] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('https://67daa14635c87309f52d5946.mockapi.io/collections')
+    .then((res) => res.json())
+    .then((json) => {
+      setCollection(json);
+    })
+    .catch((err) => {
+      console.warn(err);
+      alert('Ошибка полученя данных');
+    })
+  }, []);
   return (
-    <div>
-      Hello!
+    <div className="App">
+      <h1>Моя коллекция фотографий</h1>
+      <div className="top">
+        <ul className="tags">
+          <li className="active">Все</li>
+          <li>Горы</li>
+          <li>Море</li>
+          <li>Архитектура</li>
+          <li>Города</li>
+        </ul>
+        <input className="search-input" placeholder="Поиск по названию" />
+      </div>
+      <div className="content">
+        {collection.map((obj, index) => (
+          <Collection key={index} name={obj.name} images={obj.photos} />
+        ))}
+        
+      </div>
+      <ul className="pagination">
+        <li>1</li>
+        <li className="active">2</li>
+        <li>3</li>
+      </ul>
     </div>
-  )
+  );
 }
 
 export default App;

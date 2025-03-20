e7342cbe898d28efe414ccf117100b8bec37e4da
InnaMikitich <i.seergee@gmail.com>
2025-03-20 15:18:08 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,41 +1,62 @@
 import React from 'react';
 import './index.scss';
-import {Collection} from './Collection';
+import { Collection } from './Collection';
 
-
+const cats = [
+  { "name": "Все" },
+  { "name": "Море" },
+  { "name": "Горы" },
+  { "name": "Архитектура" },
+  { "name": "Города" }
+];
 
 function App() {
+  const [categoryId, setCategoryId] = React.useState(0);
   const [collection, setCollection] = React.useState([]);
+  const [searchValue, setSearchValue] = React.useState('');
 
   React.useEffect(() => {
-    fetch('https://67daa14635c87309f52d5946.mockapi.io/collections')
-    .then((res) => res.json())
-    .then((json) => {
-      setCollection(json);
-    })
-    .catch((err) => {
-      console.warn(err);
-      alert('Ошибка полученя данных');
-    })
-  }, []);
+    fetch(`https://67daa14635c87309f52d5946.mockapi.io/collections${categoryId ? `?category=${categoryId}` : ''}`)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return res.json();
+      })
+      .then((json) => {
+        setCollection(json);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert('Ошибка получения данных');
+      });
+  }, [categoryId]);
+
   return (
     <div className="App">
       <h1>Моя коллекция фотографий</h1>
       <div className="top">
         <ul className="tags">
-          <li className="active">Все</li>
-          <li>Горы</li>
-          <li>Море</li>
-          <li>Архитектура</li>
-          <li>Города</li>
+          {cats.map((obj, index) =>
+            <li
+              onClick={() => setCategoryId(index)}
+              className={categoryId === index ? 'active' : ''}
+              key={index}>{obj.name}</li>
+          )}
         </ul>
-        <input className="search-input" placeholder="Поиск по названию" />
+        <input
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+          className="search-input" placeholder="Поиск по названию"
+        />
       </div>
       <div className="content">
-        {collection.map((obj, index) => (
-          <Collection key={index} name={obj.name} images={obj.photos} />
-        ))}
-        
+        {Array.isArray(collection) && collection
+          .filter((obj) =>
+            obj.name.toLowerCase().includes(searchValue.toLowerCase())
+          ).map((obj, index) => (
+            <Collection key={index} name={obj.name} images={obj.photos} />
+          ))}
       </div>
       <ul className="pagination">
         <li>1</li>
